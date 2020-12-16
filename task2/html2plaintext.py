5ff858b111ebb2c3b559622da22b9a41cc3050fe
shreq <shrek.kresh@o2.pl>
2020-12-16 23:34:18 +0100

    fixed html -> plain text

--- task2/html2plaintext.py
+++ task2/html2plaintext.py
@@ -1,9 +1,18 @@
-import html2text
+import ftfy
+import bs4
 
-file = open('concatenate_text.html').read()
-txt = html2text.html2text(file)
+with open('concatenate_text.html', 'r') as file:
+    soup = bs4.BeautifulSoup(file.read(), features='html.parser')
 
-file = open('concatenate_text.txt', 'w', encoding='utf-8')
-file.write(txt)
+for s in soup(['script', 'style']):
+    s.extract()
+text = soup.get_text()
 
-file.close()
+lines = (line.strip() for line in text.splitlines())
+chunks = (phrase.strip() for line in lines for phrase in line.split('  '))
+
+text = '\n'.join(chunk for chunk in chunks if chunk)
+# text = ftfy.fix_text(text)
+
+with open('concatenate_text.txt', 'w', encoding='utf-8') as file:
+    file.write(text)
