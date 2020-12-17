78b4d86a580fc07ad391a659d606a355dcbc9315
shreq <shrek.kresh@o2.pl>
2020-12-18 00:14:25 +0100

    arff

--- task2/html2plaintext.py
+++ task2/html2plaintext.py
@@ -1,5 +1,7 @@
-import ftfy
 import bs4
+import pandas
+import arff
+import ftfy
 
 with open('concatenate_text.html', 'r') as file:
     soup = bs4.BeautifulSoup(file.read(), features='html.parser')
@@ -16,3 +18,23 @@ text = '\n'.join(chunk for chunk in chunks if chunk)
 
 with open('concatenate_text.txt', 'w', encoding='utf-8') as file:
     file.write(text)
+
+#################################################
+
+with open('concatenate_text.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+text = []
+pages = {}
+for i, line in enumerate(lines):
+    if 'Page ' in line:
+        pages[lines[i - 1]] = ' '.join(text[:-1]).replace('\n', ' ')
+        text.clear()
+        continue
+    text.append(line)
+# print(pages[u'Powiadomienie [http://ftims.p.lodz.pl/course/view.php?id=3]\n'])
+
+df = pandas.DataFrame(list(pages.keys()), columns=['title'])
+df['text'] = list(pages.values())
+df.to_csv('pages.csv', index=False)
+arff.dump('pages.arff', df.values, relation='pages', names=df.columns)
