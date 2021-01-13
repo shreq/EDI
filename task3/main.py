7960471cb0f34859fd901233f8a377e20c16a007
shreq <shrek.kresh@o2.pl>
2021-01-13 15:20:59 +0100

    polished

--- task3/main.py
+++ task3/main.py
@@ -1,26 +1,31 @@
 from random import getrandbits
-from numpy import array
+from io import StringIO
 import pandas
 
 # load result of clustering (copied from weka to the file)
-df = pandas.read_fwf('data/clusters', index_col='Attribute').drop('Full Data', axis=1)
+with open('data/clusters', 'r', encoding='utf-8') as f:
+    file = f.read().split('\n')
+file = '\n'.join(file[:1] + file[3:])
+
+df = pandas.read_fwf(StringIO(file), index_col='Attribute').drop('Full Data', axis=1)
+df.index.name = None
 
 # create random user
-user = array([bool(getrandbits(1)) for _ in range(df.shape[0])])
-# user = array([True, True,  True, False, False, False, False, False, False, False, False, False,
-#               False, False, False, False, False, False, False, False, False, False, False, False,
-#               False, False, False, False])
-# df['User'] = user
-# print(df.to_string())
-# df.drop('User', axis=1, inplace=True)
+user = pandas.Series([bool(getrandbits(1)) for _ in range(df.shape[0])], index=df.index, name='User')
+
+# print comparison
+print('Comparison of clusters and user:')
+print(pandas.merge(df, user, left_index=True, right_index=True).to_string(), '\n')
 
 # count Jaccard index
-similarities = []
-for col in df.columns:
-    union = user | df[col].values
-    intersection = user & df[col].values
-    similarities.append(sum(intersection) / sum(union))
+similarities = [sum(user & df[col].values) /
+                sum(user | df[col].values) for col in df.columns]
 
-# print recommended sites
+# print results
 assigned_cluster = str(list(similarities).index(max(similarities)))
-print(df.loc[~user & df[assigned_cluster], assigned_cluster])
+print('Similarity to groups:')
+for index, value in enumerate(similarities):
+    print(f'#{index}: \t{value}')
+print(f'Closest group is group #{assigned_cluster}\n')
+print('Recommended websites:')
+print(df.loc[~user & df[assigned_cluster], assigned_cluster].to_string())
